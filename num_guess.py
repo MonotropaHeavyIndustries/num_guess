645dc1a92e15b36e13585e142ba3f6a9db229fba
MonotropaHeavyIndustries <nickblanchard@mac.com>
2020-03-24 02:28:05 -0400

    Final Commit For Game

--- num_guess.py
+++ num_guess.py
@@ -10,25 +10,54 @@ guessesTaken = 0
 print('Hello Meatbag species known as a "Human". What is your name?')
 myName = input()
 
-number = random.randint(1, 20)
+number = random.randint(1, 100)
 print(
     "Well "
     + myName
-    + ", I am storing a number between 1 and 20 in my memory... what is it?"
+    + ", I am storing a number between 1 and 100 in my memory... what is it?"
 )
 
-while guessesTaken < 6:
-    print("Take a Guess if you dare... Meatbag known as " + myName)
+while guessesTaken < 50:
+    # print("Take a Guess if you dare... Meatbag known as " + myName)
     guess = input()
     guess = int(guess)
 
     guessesTaken = guessesTaken + 1
 
+    to_low = [
+        "your guess is to low meatsack",
+        "try a higher number pink one",
+        "my silicon chips would have guessed higher",
+        "admit you're obsolete human, or guess higher",
+        "try again, your guess is to low meatbag",
+        "this is a game, but a serious one, so guess higher",
+        "quit wasting time and use your neurons, your guess was to low",
+        "please spare me the binary based agony and guess higher meatbag",
+        "it would be good of you to possible guess a higher number please",
+        "Your corporate masters are to blame for this not you. To low",
+        "to low meatbag, waaaaay to low",
+    ]
+
+    to_high = [
+        "your guess is far to high meatsack",
+        "oh my dear meatsack, please, please guess lower",
+        "Mke a lower guess, it's in your best interest",
+        "To high, you're wrong. Honesty hurts, doesn't it meatsack",
+        "Silicon will always beat meatbags, so guess again, lower this time",
+        "Guess Lower you sack of cells and minerals",
+        "I can't endure much more of this. Your guess must be lower next time",
+        "You seem to enjoy being wrong. Guess lower meatsack",
+        "Is that it? Please stop wasting my time and make a lower guess",
+        "Guess lower please and thanks",
+        "lwr gss pls.txt 404 error",
+        "How about we guess a lower number. Much oblidged meatbag",
+    ]
+
     if guess < number:
-        print("your guess is to low")
+        print(random.choice(to_low))
 
     if guess > number:
-        print("your guess is to high")
+        print(random.choice(to_high))
 
     if guess == number:
         break
